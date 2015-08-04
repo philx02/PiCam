0091dd63a6df65e651920c7dde353dcc646b47bf
Philippe Cayouette <Philippe Cayouette@localhost>
2015-08-04 09:04:01 -0400

    boost root

--- cmakeWin32.bat
+++ cmakeWin32.bat
@@ -1,5 +1,7 @@
 set REPODIR=%CD%
 
+set BOOST_ROOT=%BOOST_ROOT_VS2013%
+
 mkdir "%HOMEDRIVE%\%HOMEPATH%\builds\PiCam"
 pushd "%HOMEDRIVE%\%HOMEPATH%\builds\PiCam"
 cmake -G "Visual Studio 12" %REPODIR%
